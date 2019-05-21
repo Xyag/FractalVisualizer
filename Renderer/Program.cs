c8a8463fa946095d3ea08787633a9a5684c264b0
jim <xyagonline@gmail.com>
2019-05-20 22:53:52 -0500

    beginning UI

---
+++ Renderer/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FractalVisualizer
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Color[] graphColors = {Color.Black, Color.FromArgb(0,0,100), Color.FromArgb(0,50,200), Color.Red, Color.Yellow, Color.White};
+            //Color[] graphColors = {Color.Black, Color.DarkRed, Color.Purple, Color.FromArgb(196,0,52)};
+            
+            double xMin = -2;//Convert.ToDouble(args[0]);
+            double yMin = -1.5;//Convert.ToDouble(args[1]);
+            double xMax = 1;//Convert.ToDouble(args[2]);
+            double yMax = 1.5;//Convert.ToDouble(args[3]);
+            string imagePath = @"C:\Users\Jim\Pictures\Fractals\";//@"C:\Users\Public\Pictures\Mandelbrot\debug\";//args[4];
+
+            Bitmap b = new Bitmap(720,720);
+            int maxIterations = 100;
+
+            Mandelbrot man = new Mandelbrot(graphColors);
+            //man.Graph(new Complex(xMin,yMin), new Complex(xMax,yMax), maxIterations, ref b,2);
+
+            b.Save(imagePath + getTimedName() + ".png");
+
+            //Animator.Animate(new Complex(-1,-0.5), new Complex(1,0.5), new Complex(1,-0.0005), new Complex(1.0005,0.0005), 100, graphColors, 50);
+            Animator.AnimatePowerThreaded(man, new Complex(-2,-1.5), new Complex(1,1.5), 0, 10, 20, 4, 900);
+        }
+
+        static string getTimedName()
+        {
+            return System.DateTime.Now.Hour + "-" + System.DateTime.Now.Minute + "-" + System.DateTime.Now.Second + "-" + System.DateTime.Now.Millisecond;
+        }
+
+    }
+
+    public struct Complex
+    {
+        public double real;
+        public double imaginary;
+
+        public Complex(double real, double imaginary) {this.real = real; this.imaginary = imaginary;}
+        public double SqrMagnitude()
+        {
+            return real*real+imaginary*imaginary;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} + {1}i",real,imaginary);
+        }
+        public static Complex Exp(Complex num)
+        {
+            double expReal = Math.Exp(num.real);
+            return new Complex(expReal*Math.Cos(num.imaginary),expReal*Math.Sin(num.imaginary));
+        }
+        public static Complex Log(Complex num)
+        {
+            double theta = Math.Atan2(num.imaginary,num.real);
+            double r = Math.Sqrt(num.SqrMagnitude());
+            return new Complex(Math.Log(r),theta);
+        }
+        public static Complex operator +(Complex lhs, Complex rhs)
+        {
+            return new Complex(lhs.real+rhs.real, lhs.imaginary+rhs.imaginary);
+        }
+        public static Complex operator -(Complex lhs, Complex rhs)
+        {
+            return new Complex(lhs.real-rhs.real, lhs.imaginary-rhs.imaginary);
+        }
+        public static Complex operator *(Complex lhs, Complex rhs)
+        {
+            return new Complex(lhs.real*rhs.real-lhs.imaginary*rhs.imaginary,lhs.real*rhs.imaginary+lhs.imaginary*rhs.real);
+        }
+        public static Complex operator *(double a, Complex v)
+        {
+            return new Complex(a*v.real,a*v.imaginary);
+        }
+        public static Complex operator /(Complex num, Complex denom)
+        {
+            double scale = denom.imaginary*denom.imaginary+denom.real*denom.real;
+            return new Complex ((num.real*denom.real+num.imaginary*denom.imaginary)/scale,(num.imaginary*denom.real-num.real*denom.imaginary)/scale);
+        }
+    }
+}
---
+++ UI/FractalUI/FractalUI/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace FractalUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxControls = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRender = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelCores = new System.Windows.Forms.Label();
+            this.buttonSaveImage = new System.Windows.Forms.Button();
+            this.comboBoxFractals = new System.Windows.Forms.ComboBox();
+            this.groupBoxControls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxControls
+            // 
+            this.groupBoxControls.Controls.Add(this.comboBoxFractals);
+            this.groupBoxControls.Controls.Add(this.buttonSaveImage);
+            this.groupBoxControls.Controls.Add(this.labelCores);
+            this.groupBoxControls.Controls.Add(this.textBox1);
+            this.groupBoxControls.Controls.Add(this.buttonRender);
+            this.groupBoxControls.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxControls.Name = "groupBoxControls";
+            this.groupBoxControls.Size = new System.Drawing.Size(150, 691);
+            this.groupBoxControls.TabIndex = 0;
+            this.groupBoxControls.TabStop = false;
+            this.groupBoxControls.Text = "Controls";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(170, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(720, 720);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonRender
+            // 
+            this.buttonRender.Location = new System.Drawing.Point(7, 20);
+            this.buttonRender.Name = "buttonRender";
+            this.buttonRender.Size = new System.Drawing.Size(137, 23);
+            this.buttonRender.TabIndex = 0;
+            this.buttonRender.Text = "Render";
+            this.buttonRender.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 710);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(151, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 672);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelCores
+            // 
+            this.labelCores.AutoSize = true;
+            this.labelCores.Location = new System.Drawing.Point(6, 675);
+            this.labelCores.Name = "labelCores";
+            this.labelCores.Size = new System.Drawing.Size(34, 13);
+            this.labelCores.TabIndex = 2;
+            this.labelCores.Text = "Cores";
+            // 
+            // buttonSaveImage
+            // 
+            this.buttonSaveImage.Location = new System.Drawing.Point(9, 50);
+            this.buttonSaveImage.Name = "buttonSaveImage";
+            this.buttonSaveImage.Size = new System.Drawing.Size(135, 23);
+            this.buttonSaveImage.TabIndex = 3;
+            this.buttonSaveImage.Text = "Save Image";
+            this.buttonSaveImage.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxFractals
+            // 
+            this.comboBoxFractals.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFractals.FormattingEnabled = true;
+            this.comboBoxFractals.Items.AddRange(new object[] {
+            "Mandelbrot",
+            "1",
+            "2",
+            "3",
+            "Test"});
+            this.comboBoxFractals.Location = new System.Drawing.Point(9, 79);
+            this.comboBoxFractals.Name = "comboBoxFractals";
+            this.comboBoxFractals.Size = new System.Drawing.Size(135, 21);
+            this.comboBoxFractals.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 741);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBoxControls);
+            this.Name = "Form1";
+            this.Text = "Fractual Visualizer";
+            this.groupBoxControls.ResumeLayout(false);
+            this.groupBoxControls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxControls;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonSaveImage;
+        private System.Windows.Forms.Label labelCores;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRender;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBoxFractals;
+    }
+}
+
